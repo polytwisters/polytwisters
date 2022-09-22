dfa30e9a060e6b9158122f97a67a51730c8c033c
Nathan Ho <nathan.waikin.ho@gmail.com>
2022-09-21 19:11:42 -0700

    Position cylinders for pentadyster

--- polytwisters.py
+++ polytwisters.py
@@ -1,27 +1,11 @@
 import math
 import bpy
+import mathutils
 
 EPSILON = 1e-10
 LARGE = 10e3
 
-
-def create_cycloplane(theta, z, rotation):
-    # if abs(theta - math.pi / 2) < EPSILON:
-
-    scale_x = 1 / math.cos(theta)
-    translate_x = z * math.tan(theta)
-
-    bpy.ops.object.select_all(action='DESELECT')
-    bpy.ops.mesh.primitive_cylinder_add(
-        radius=1,
-        depth=LARGE,
-        vertices=32,
-        location=(0, 0, 0),
-        scale=(1, 1, 1),
-    )
-    bpy.ops.transform.resize(value=(scale_x, 1, 1))
-    bpy.ops.transform.translate(value=(translate_x, 0, 0))
-
+def rotate_about_axis(axis, angle):
     # See https://stackoverflow.com/a/67697363.
     for area in bpy.context.screen.areas:
         if area.type == "VIEW_3D":
@@ -30,21 +14,45 @@ def create_cycloplane(theta, z, rotation):
     else:
         raise RuntimeError("VIEW_3D area not found")
 
-    bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
-
+    bpy.ops.object.origin_set(type="ORIGIN_CURSOR")
     with bpy.context.temp_override(area=view_3d):
-        axes = ["X", "Y", "Z"]
-        for i in range(3):
-            bpy.ops.transform.rotate(
-                value=rotation[i],
-                orient_axis=axes[i],
-            )
+        bpy.ops.transform.rotate(
+            value=angle,
+            orient_axis=axis,
+        )
+
+
+def create_cycloplane(theta, z):
+    # if abs(theta - math.pi / 2) < EPSILON:
+
+    scale_x = 1 / math.cos(theta)
+    translate_x = z * math.tan(theta)
+
+    bpy.ops.object.select_all(action="DESELECT")
+    bpy.ops.mesh.primitive_cylinder_add(
+        radius=1,
+        depth=LARGE,
+        vertices=32,
+        location=(0, 0, 0),
+        scale=(1, 1, 1),
+    )
+
+    # The cylinder is normally about the Z-axis. Rotate about the X-axis to
+    # change Z-axis to Y-axis and match with the original "cyl" object.
+    rotate_about_axis("X", math.pi / 2)
+
+    bpy.ops.transform.resize(value=(scale_x, 1, 1))
+    rotate_about_axis("X", -theta)
+    bpy.ops.transform.translate(value=(translate_x, 0, 0))
 
 
 if __name__ == "__main__":
     # Delete the default cube.
     bpy.ops.object.delete(use_global=False)
 
-    z = 0.5
-    create_cycloplane(0, z, (0.1, 0, 0)) 
-    create_cycloplane(1.3, z, (1.4, 1.4, 0)) 
+    z = 0.1
+    theta = math.pi / 4
+    n = 5
+    for k in range(n):
+        create_cycloplane(theta, z) 
+        rotate_about_axis("Y", k * 2 * math.pi / n)
