eb244e3bd7efa5c83a2e7f1f7f05e62c70757c16
Nathan Ho <nathan.waikin.ho@gmail.com>
2022-09-22 16:20:13 -0700

    Refactor Boolean code into a function

--- polytwisters.py
+++ polytwisters.py
@@ -29,8 +29,6 @@ def create_cycloplane(theta, z):
     in Bowers' original code."""
     # if abs(theta - math.pi / 2) < EPSILON:
 
-    scale_x = 1 / math.cos(theta)
-    translate_x = z * math.tan(theta)
 
     deselect_all()
     bpy.ops.mesh.primitive_cylinder_add(
@@ -41,13 +39,37 @@ def create_cycloplane(theta, z):
         scale=(1, 1, 1),
     )
 
-    # The cylinder is normally about the Z-axis. Rotate about the X-axis to
-    # change Z-axis to Y-axis and match with the original "cyl" object.
+    # The cylinder is along the Z-axis. Rotate about the X-axis to
+    # change Z-axis to Y-axis and match with the original "cyl" object
+    # in Bowers' POV-Ray code.
     rotate_about_axis("X", math.pi / 2)
 
+    scale_x = 1 / math.cos(theta)
     bpy.ops.transform.resize(value=(scale_x, 1, 1))
     rotate_about_axis("X", -theta)
+    translate_x = z * math.tan(theta)
     bpy.ops.transform.translate(value=(translate_x, 0, 0))
+
+def intersect(objects):
+    """Given a list of Blender objects, compute the intersection of all of
+    them by creating and applying Boolean modifiers on the first object, then
+    deleting the rest of the objects.
+
+    It is assumed that there are no other modifiers on the first object."""
+    first = objects[0]
+    for other in objects[1:]:
+        deselect_all()
+        first.select_set(True)
+        bpy.context.view_layer.objects.active = first
+        bpy.ops.object.modifier_add(type="BOOLEAN")
+        modifier = bpy.context.object.modifiers.values()[0]
+        modifier.operation = "INTERSECT"
+        modifier.object = other
+        bpy.ops.object.modifier_apply(modifier=modifier.name)
+
+        deselect_all()
+        other.select_set(True)
+        bpy.ops.object.delete()
 
 
 if __name__ == "__main__":
@@ -64,17 +86,4 @@ if __name__ == "__main__":
         rotate_about_axis("Y", i * 2 * math.pi / n)
         cycloplanes.append(bpy.context.object)
 
-    for i in range(1, len(cycloplanes)):
-        deselect_all()
-        cycloplanes[0].select_set(True)
-        bpy.context.view_layer.objects.active = cycloplanes[0]
-        bpy.ops.object.modifier_add(type="BOOLEAN")
-        modifier = bpy.context.object.modifiers["Boolean"]
-        modifier.operation = "INTERSECT"
-        other = cycloplanes[i]
-        modifier.object = other
-        bpy.ops.object.modifier_apply(modifier=modifier.name)
-
-        deselect_all()
-        other.select_set(True)
-        bpy.ops.object.delete()
+    intersect(cycloplanes)
