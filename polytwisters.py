58f46ba471703270d76c96cb3150231fea8f97fe
Nathan Ho <nathan.waikin.ho@gmail.com>
2022-09-21 18:07:29 -0700

    Get rotation working

--- polytwisters.py
+++ polytwisters.py
@@ -3,7 +3,6 @@ import bpy
 
 EPSILON = 1e-10
 LARGE = 10e3
-IDENTITY_MATRIX = ((1, 0, 0), (0, 1, 0), (0, 0, 1))
 
 
 def create_cycloplane(theta, z, rotation):
@@ -20,43 +19,32 @@ def create_cycloplane(theta, z, rotation):
         location=(0, 0, 0),
         scale=(1, 1, 1),
     )
-    bpy.ops.transform.resize(
-        value=(scale_x, 1, 1),
-        orient_matrix=IDENTITY_MATRIX,
-        constraint_axis=(True, False, False),
-    )
-    bpy.ops.transform.translate(
-        value=(translate_x, 0, 0),
-        orient_matrix=IDENTITY_MATRIX,
-        constraint_axis=(True, False, False),
-    )
+    bpy.ops.transform.resize(value=(scale_x, 1, 1))
+    bpy.ops.transform.translate(value=(translate_x, 0, 0))
 
-    # bpy.ops.view3d.snap_cursor_to_center()
-    # bpy.context.scene.tool_settings.transform_pivot_point = 'CURSOR'
-
-    context = bpy.context.copy()
+    # See https://stackoverflow.com/a/67697363.
     for area in bpy.context.screen.areas:
         if area.type == "VIEW_3D":
-            context["area"] = area
+            view_3d = area
             break 
     else:
         raise RuntimeError("VIEW_3D area not found")
 
-    axes = ["X", "Y", "Z"]
-    for i in range(3):
-        bpy.ops.transform.rotate(
-            context,
-            value=rotation[i],
-            orient_axis=axes[i],
-            orient_type='GLOBAL',
-            orient_matrix=IDENTITY_MATRIX,
-        )
+    bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
+
+    with bpy.context.temp_override(area=view_3d):
+        axes = ["X", "Y", "Z"]
+        for i in range(3):
+            bpy.ops.transform.rotate(
+                value=rotation[i],
+                orient_axis=axes[i],
+            )
 
 
 if __name__ == "__main__":
     # Delete the default cube.
     bpy.ops.object.delete(use_global=False)
 
-    z = 0.1
+    z = 0.5
     create_cycloplane(0, z, (0.1, 0, 0)) 
-    create_cycloplane(0.3, z, (1.4, 0, 0)) 
+    create_cycloplane(1.3, z, (1.4, 1.4, 0)) 
