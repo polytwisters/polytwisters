420a7536af3b8012949482b88f78f180737a5311
Nathan Ho <nathan.waikin.ho@gmail.com>
2022-11-06 22:57:25 -0800

    Replace hill climb with scipy.optimize.minimize

--- cycloplanes.py
+++ cycloplanes.py
@@ -1,13 +1,14 @@
-import math
 import random
 
+import numpy as np
 import matplotlib.pyplot as plt
+import scipy.optimize
 
 
 def rotate_2d(x, y, theta):
     return (
-        x * math.cos(theta) - y * math.sin(theta),
-        x * math.sin(theta) + y * math.cos(theta)
+        x * np.cos(theta) - y * np.sin(theta),
+        x * np.sin(theta) + y * np.cos(theta)
     )
 
 
@@ -19,7 +20,7 @@ def get_cycloplane_error(point, zenith, azimuth, radius=1.0):
     z, w = rotate_2d(z, w, -phi)
     x, w = rotate_2d(x, w, -theta)
     y, z = rotate_2d(y, z, -theta)
-    signed_distance = math.hypot(x, y) - radius
+    signed_distance = np.hypot(x, y) - radius
     return signed_distance * signed_distance
 
 
@@ -30,49 +31,38 @@ def get_summed_cycloplane_error(point, cycloplane_specs):
     ])
 
 
-def find_minimum(start, cycloplane_specs, step=0.001):
-    point = list(start)
-    best_error = math.inf
-    while best_error > 0.05:
-        new_points = []
-        for i in range(len(point)):
-            new_point_1 = point[:]
-            new_point_1[i] += step
-            new_points.append(new_point_1)
-            new_point_2 = point[:]
-            new_point_2[i] -= step
-            new_points.append(new_point_2)
-        for new_point in new_points:
-            error = get_summed_cycloplane_error(new_point, cycloplane_specs)
-            if error < best_error:
-                point = new_point
-                best_error = error
-    return point
+def find_minimum(start, cycloplane_specs):
+    temp = scipy.optimize.minimize(
+        lambda x: get_summed_cycloplane_error(x, cycloplane_specs),
+        start
+    )
+    return temp.x
 
 
 def main():
     cycloplane_specs = [
         (0.0, 0.0),
-        (math.pi / 2, 0.0),
-        (math.pi / 2, math.pi / 2),
+        (np.pi / 2, 0.0),
+        (np.pi / 2, np.pi / 2),
     ]
-    X = []
-    Y = []
-    radii = []
-    for i in range(100):
+    count = 1000
+    points = np.zeros((count, 4))
+    radii = np.zeros(count)
+    for i in range(count):
         point = [(random.random() * 2 - 1) * 3 for i in range(4)]
         minimum = find_minimum(point, cycloplane_specs)
         error = get_summed_cycloplane_error(minimum, cycloplane_specs)
-        X.append(minimum[0])
-        Y.append(minimum[1])
-        radius = math.hypot(*minimum)
-        radii.append(radius)
-        print(minimum)
-        print(f"Radius = {radius:.5f}, error = {error:.5f}")
-    average_radius = sum(radii) / len(radii)
-    print(f"Average radius = {average_radius:.5f}")
+        radius = np.sqrt(np.sum(np.square(minimum)))
+        radii[i] = radius
+        points[i, :] = minimum
+    average_radius = np.mean(radii)
+    print(f"Average radius = {average_radius:.5}")
+    standard_deviation = np.std(radii)
+    print(f"Standard deviation of radii = {standard_deviation:.5}")
 
-    plt.scatter(X, Y, 1)
+    plt.scatter(points[:, 0], points[:, 1], 1)
+    plt.show()
+    plt.scatter(points[:, 2], points[:, 3], 1)
     plt.show()
 
 
