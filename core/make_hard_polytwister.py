012cb2579999ae55ee36b5460e03101ba4504bf4
Nathan Ho <nathan.waikin.ho@gmail.com>
2023-09-27 14:10:42 -0700

    Add SVG output

--- core/make_hard_polytwister.py
+++ core/make_hard_polytwister.py
@@ -2,18 +2,27 @@ import argparse
 import math
 import logging
 import pathlib
+from io import StringIO
 
 import cadquery
 import cadquery.utils
+from OCP.gp import gp_Ax2, gp_Pnt, gp_Dir
+from OCP.BRepLib import BRepLib
+from OCP.HLRBRep import HLRBRep_Algo, HLRBRep_HLRToShape
+from OCP.HLRAlgo import HLRAlgo_Projector
+from OCP.GCPnts import GCPnts_QuasiUniformDeflection
 
 import hard_polytwisters
+
+TOLERANCE = 1e-6
+DISCRETIZATION_TOLERANCE = 1e-3
 
 LARGE = 1000.0
 EPSILON = 1e-3
 
 
 # https://github.com/CadQuery/cadquery/issues/638
-def scale_cadquery_workplane(workplane: cadquery.Workplane, x: float, y: float, z: float) -> cadquery.Workplane:
+def scale_workplane(workplane: cadquery.Workplane, x: float, y: float, z: float) -> cadquery.Workplane:
     matrix = cadquery.Matrix([
         [x, 0, 0, 0],
         [0, y, 0, 0],
@@ -24,6 +33,10 @@ def scale_cadquery_workplane(workplane: cadquery.Workplane, x: float, y: float, 
         object_.transformGeometry(matrix) if isinstance(object_, cadquery.Shape) else object_
         for object_ in workplane.objects
     ])
+
+
+def scale_workplane_uniform(workplane: cadquery.Workplane, amount: float) -> cadquery.Workplane:
+    return scale_workplane(workplane, amount, amount, amount)
 
 
 def create_cycloplane(w, zenith, azimuth):
@@ -51,7 +64,7 @@ def create_cycloplane(w, zenith, azimuth):
     part = part.rotate((0.0, 0.0, 0.0), (1.0, 0.0, 0.0), math.degrees(math.pi / 2))
 
     scale_x = 1 / math.cos(theta)
-    part = scale_cadquery_workplane(part, scale_x, 1.0, 1.0)
+    part = scale_workplane(part, scale_x, 1.0, 1.0)
     translate_x = w * math.tan(theta)
     part = part.translate((translate_x, 0, 0))
 
@@ -92,7 +105,13 @@ def bubble(x):
     return [x]
 
 
+def is_empty(part):
+    return len(part.solids().vals()) == 0
+
+
 def safe_intersect(part_1, part_2):
+    """Compute the intersection of two cadquery.Workplanes, but don't throw an error for empty
+    inputs."""
     try:
         return part_1.intersect(part_2)
     except ValueError:
@@ -100,16 +119,19 @@ def safe_intersect(part_1, part_2):
 
 
 def safe_cut(part_1, part_2):
-    try:
-        return part_1.cut(part_2)
-    except ValueError:
+    """Compute the difference of two cadquery.Workplanes, but don't throw an error for empty inputs."""
+    if is_empty(part_1):
         return cadquery.Workplane()
+    elif is_empty(part_2):
+        return part_1
+    return part_1.cut(part_2)
 
 
 def safe_union(part_1, part_2):
-    if len(part_1.solids().vals()) == 0:
+    """Compute the union of two cadquery.Workplanes, but don't throw an error for empty inputs."""
+    if is_empty(part_1):
         return part_2
-    elif len(part_2.solids().vals()) == 0:
+    elif is_empty(part_2):
         return part_1
     return part_1.union(part_2)
 
@@ -167,14 +189,15 @@ class Realizer:
             raise ValueError(f'Invalid node type {type_}')
 
 
-def compute_polytwister_cross_section(polytwister, w, scale=1.0, tolerance=0.1, angular_tolerance=0.1):
-    """Given a hard polytwister spec and a W coordinate, find the cross section.
-
-    Return a tuple comprising a set of vertices and triangles. The vertices are a list of
-    cadquery.Vectors, and the triangles a list of 3-tuples of ints indexing into the vertex list.
-    Vertex indices start at 0."""
+def make_polytwister_cross_section(polytwister, w):
     workplane = Realizer(w).realize(polytwister)
-    workplane = scale_cadquery_workplane(workplane, scale, scale, scale)
+    return workplane
+
+
+def discretize_workplane(workplane, tolerance=0.1, angular_tolerance=0.1):
+    """Given a workplane containing a single shape, return a tuple comprising a set of vertices and
+    triangles. The vertices are a list of cadquery.Vectors, and the triangles a list of 3-tuples of
+    ints indexing into the vertex list. Vertex indices start at 0."""
     shapes = [thing for thing in workplane.objects if isinstance(thing, cadquery.Shape)]
     if len(shapes) == 0:
         return [], []
@@ -194,7 +217,8 @@ def write_mesh_as_obj(mesh, file):
 
 
 def get_max_distance_from_origin(polytwister, w):
-    vertices, __ = compute_polytwister_cross_section(polytwister, w)
+    workplane = make_polytwister_cross_section(polytwister, w)
+    vertices, __ = discretize_workplane(workplane)
     if len(vertices) == 0:
         return 0.0
     return max([abs(vertex) for vertex in vertices])
@@ -271,6 +295,105 @@ def get_scale_and_max_w(polytwister):
     return scale, max_w_upper_bound
 
 
+def convert_edge_to_polyline(e):
+    curve = e._geomAdaptor()  # adapt the edge into curve
+    start = curve.FirstParameter()
+    end = curve.LastParameter()
+    points = GCPnts_QuasiUniformDeflection(curve, DISCRETIZATION_TOLERANCE, start, end)
+    if points.IsDone():
+        points = [points.Value(i + 1) for i in range(points.NbPoints())]
+        points = [(point.X(), point.Y()) for point in points]
+        return points
+    return points
+
+
+def export_svg(workplane, normalize=False):
+    shape = workplane.val()
+
+    hlr = HLRBRep_Algo()
+    hlr.Add(shape.wrapped)
+    projectionDir = (-1.75, 1.1, 5)
+    coordinate_system = gp_Ax2(gp_Pnt(), gp_Dir(*projectionDir))
+    projector = HLRAlgo_Projector(coordinate_system)
+    hlr.Projector(projector)
+    hlr.Update()
+    hlr.Hide()
+
+    hlr_shapes = HLRBRep_HLRToShape(hlr)
+
+    visible = []
+    visible_sharp_edges = hlr_shapes.VCompound()
+    if not visible_sharp_edges.IsNull():
+        visible.append(visible_sharp_edges)
+
+    visible_smooth_edges = hlr_shapes.Rg1LineVCompound()
+    if not visible_smooth_edges.IsNull():
+        visible.append(visible_smooth_edges)
+
+    visible_contour_edges = hlr_shapes.OutLineVCompound()
+    if not visible_contour_edges.IsNull():
+        visible.append(visible_contour_edges)
+
+    for el in visible:
+        BRepLib.BuildCurves3d_s(el, TOLERANCE)
+
+    canvas_size = 500.0
+    border_size = 20.0
+    figure_size = canvas_size - 2 * border_size
+
+    visible = [cadquery.Shape(x) for x in visible]
+    paths = []
+    for shape in visible:
+        for edge in shape.Edges():
+            paths.append(convert_edge_to_polyline(edge))
+
+    if normalize:
+        points = [point for path in paths for point in path]
+        x_coordinates, y_coordinates = zip(*points)
+        x_min = min(x_coordinates)
+        x_max = max(x_coordinates)
+        y_min = min(y_coordinates)
+        y_max = max(y_coordinates)
+        max_dimension = max(x_max - x_min, y_max - y_min)
+        scale = figure_size / max_dimension
+    else:
+        scale = figure_size / 2
+    center = canvas_size / 2
+
+    # Note: Y-axis is flipped here.
+    paths = [
+        [(center + x * scale, center - y * scale) for x, y in path]
+        for path in paths
+    ]
+
+    svg_paths = [
+        " ".join([f"{'M' if i == 0 else 'L'} {x} {y}" for i, (x, y) in enumerate(path)])
+        for path in paths
+    ]
+    svg_paths = [f'<path d="{path}" />' for path in svg_paths]
+
+    return f"""<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+    <svg
+        xmlns:svg="http://www.w3.org/2000/svg"
+        xmlns="http://www.w3.org/2000/svg"
+        width="{canvas_size}"
+        height="{canvas_size}">
+        <g stroke="black" fill="none" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
+            {"".join(svg_paths)}
+        </g>
+   </svg>
+    """
+
+
+def normalize_mesh(mesh):
+    vertices, triangles = mesh
+    if len(vertices) == 0:
+        return mesh
+    scale = 1 / max([abs(vertex) for vertex in vertices])
+    vertices = [vertex * scale for vertex in vertices]
+    return vertices, triangles
+
+
 def main():
     logging.basicConfig(level=logging.DEBUG)
 
@@ -291,9 +414,14 @@ def main():
         help="Number of frames.",
     )
     parser.add_argument(
+        "--svg",
+        action="store_true",
+        help="If provided, write SVG files instead of OBJ files.",
+    )
+    parser.add_argument(
         "out_dir",
         type=str,
-        help="Output directory of wavefront OBJ file(s)."
+        help="Output directory of file(s)."
     )
     args = parser.parse_args()
 
@@ -309,20 +437,33 @@ def main():
     out_dir = pathlib.Path(args.out_dir)
     out_dir.mkdir(exist_ok=True, parents=True)
 
+    def do_action(w, out_file_stem, normalize=False):
+        workplane = make_polytwister_cross_section(polytwister, w)
+        if args.svg:
+            out_file_name = out_dir / (out_file_stem + ".svg")
+            svg_document = export_svg(workplane, normalize=True)
+            with open(out_dir / out_file_name, "w") as f:
+                f.write(svg_document)
+        else:
+            out_file_name = out_dir / (out_file_stem + ".obj")
+            mesh = discretize_workplane(workplane)
+            if normalize:
+                mesh = normalize_mesh(mesh)
+            with open(out_dir / out_file_name, "w") as f:
+                write_mesh_as_obj(mesh, f)
+
     if args.w is not None:
-        mesh = compute_polytwister_cross_section(polytwister, args.w)
-        with open(out_dir / "out.obj", "w") as f:
-            write_mesh_as_obj(mesh, f)
+        do_action(args.w, "out", normalize=True)
     else:
         scale, max_w = get_scale_and_max_w(polytwister)
         num_frames = args.n
         num_digits = int(math.ceil(math.log10(num_frames)))
         for i in range(num_frames):
             logging.debug(f"Rendering frame {i}.")
-            w = -max_w + max_w * 2 * i / (num_frames - 1)
-            mesh = compute_polytwister_cross_section(polytwister, w)
-            with open(out_dir / f"out_{str(i).rjust(num_digits, '0')}.obj", "w") as f:
-                write_mesh_as_obj(mesh, f)
+            do_action(
+                -max_w + max_w * 2 * i / (num_frames - 1),
+                f"out_{str(i).rjust(num_digits, '0')}",
+            )
 
 
 if __name__ == "__main__":
