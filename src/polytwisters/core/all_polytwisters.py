70e430783c62777980699772a09cfa30875dd089
Nathan Ho <nathan.waikin.ho@gmail.com>
2024-03-31 13:37:35 -0700

    Fix some issues with get_all_polytwisters

--- src/polytwisters/core/all_polytwisters.py
+++ src/polytwisters/core/all_polytwisters.py
@@ -4,12 +4,11 @@ from . import hard_polytwisters
 
 
 def get_all_polytwisters():
-    normalized_name = common.normalize_polytwister_name(name)
     result = {}
     result.update(soft_polytwisters.get_all_soft_polytwisters())
     result.update(hard_polytwisters.get_all_hard_polytwisters())
-    return result[normalized_name]
+    return result
 
 
 def get_polytwister(name):
-    return all_polytwisters()[name]
+    return get_all_polytwisters()[name]
